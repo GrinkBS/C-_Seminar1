4e99ce7e028cb0140d4972cf864cd306d9e0bfd7
Vyacheslav <grinksic@gmail.com>
2022-08-05 12:59:04 +0300

    add HomeWork (task5 - task8)

---
+++ task5/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите число A: ");
+int numbera = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int numberb = Convert.ToInt32(Console.ReadLine());
+
+if(numbera > numberb)
+{
+    Console.Write ("max = ");
+    Console.WriteLine (numbera);
+    Console.Write ("min = ");
+    Console.Write (numberb);
+}
+else
+{
+    Console.Write ("max = ");
+    Console.WriteLine (numberb);
+    Console.Write ("min = ");
+    Console.Write (numbera);
+}
---
+++ task6/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите число A: ");
+int numbera = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int numberb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число C: ");
+int numberc = Convert.ToInt32(Console.ReadLine());
+
+if(numbera > numberb & numbera > numberc)
+{
+    Console.Write ("max = ");
+    Console.WriteLine (numbera);
+}
+else
+{
+    if(numberb > numbera & numberb > numberc)
+    {
+        Console.Write ("max = ");
+        Console.WriteLine (numberb); 
+    }
+    else
+    {
+        Console.Write ("max = ");
+        Console.WriteLine (numberc); 
+    }
+}
